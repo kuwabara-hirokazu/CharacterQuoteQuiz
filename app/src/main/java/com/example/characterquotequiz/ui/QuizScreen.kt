d83880070651287fe0faf47fc81219f1df672fd9
kuwabara <kuwabara@sansan.com>
2022-06-20 23:14:02 +0900

    レイアウト調整

--- app/src/main/java/com/example/characterquotequiz/ui/QuizScreen.kt
+++ app/src/main/java/com/example/characterquotequiz/ui/QuizScreen.kt
@@ -2,18 +2,17 @@ package com.example.characterquotequiz.ui
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.characterquotequiz.ui.model.Quiz
@@ -32,57 +31,63 @@ fun QuizListScreen(viewModel: QuizViewModel) {
 @Composable
 fun QuizItem(quiz: Quiz, onQuoteTranslate: (String) -> Unit) {
     Column(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .padding(top = 16.dp)
+        modifier = Modifier.padding(16.dp)
     ) {
         Row {
-            Text(text = "Q${quiz.id}.")
+            Text(text = "Q${quiz.id}.", fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.width(4.dp))
             Text(text = quiz.quote)
         }
         Spacer(modifier = Modifier.height(12.dp))
 
         var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-        )
 
-        Column {
-            Button(
-                enabled = quiz.translateQuote == null,
-                onClick = { onQuoteTranslate(quiz.quote) },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Blue,
-                    contentColor = Color.White,
-                    disabledBackgroundColor = Color.Gray,
-                    disabledContentColor = Color.White
-                ),
-                modifier = Modifier.width(100.dp)
-            ) {
-                Text(text = "翻訳")
-            }
-            quiz.translateQuote?.let { Text(text = it, modifier = Modifier.fillMaxWidth()) }
-            Spacer(modifier = Modifier.height(12.dp))
-
-            Button(
-                onClick = { isExpanded = !isExpanded },
-                colors = ButtonDefaults.textButtonColors(
-                    backgroundColor = Color.Red,
-                    contentColor = Color.White
-                ),
-                modifier = Modifier.width(100.dp)
-            ) {
-                Text(text = "Ans.")
+        Column(Modifier.padding(horizontal = 8.dp)) {
+            Row {
+                Button(
+                    enabled = quiz.translateQuote == null,
+                    onClick = { onQuoteTranslate(quiz.quote) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue,
+                        contentColor = Color.White,
+                        disabledBackgroundColor = Color.Gray,
+                        disabledContentColor = Color.White
+                    ),
+                    modifier = Modifier.width(100.dp)
+                ) {
+                    Text(text = "翻訳")
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(
+                    onClick = { isExpanded = !isExpanded },
+                    colors = ButtonDefaults.textButtonColors(
+                        backgroundColor = Color.Red,
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier.width(100.dp)
+                ) {
+                    Text(text = "Answer")
+                }
             }
             Column(
                 modifier = Modifier
                     .animateContentSize()
-                    .padding(vertical = 8.dp)
                     .fillMaxWidth()
             ) {
+                quiz.translateQuote?.let {
+                    Text(
+                        text = it, modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp)
+                    )
+                }
                 if (isExpanded) {
-                    Text(text = quiz.character, color = surfaceColor)
+                    Text(
+                        text = quiz.character,
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(vertical = 12.dp)
+                    )
                     CharacterImage(url = quiz.characterUrl)
                 }
             }
@@ -97,6 +102,7 @@ fun CharacterImage(url: String) {
         update = { webView ->
             webView.webViewClient = WebViewClient()
             webView.loadUrl(url)
-        }
+        },
+        modifier = Modifier.height(320.dp)
     )
 }

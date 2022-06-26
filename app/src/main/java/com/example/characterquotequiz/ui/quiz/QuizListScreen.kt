2076e22485c0ea9a5ee196145c7cc624696c33f8
kuwabara <kuwabara@sansan.com>
2022-06-26 11:39:29 +0900

    LazyColumnにkeyを紐づける

--- app/src/main/java/com/example/characterquotequiz/ui/quiz/QuizListScreen.kt
+++ app/src/main/java/com/example/characterquotequiz/ui/quiz/QuizListScreen.kt
@@ -6,14 +6,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun QuizListScreen(viewModel: QuizViewModel, canLoading: Boolean) {
     val quizList by viewModel.quizList.observeAsState(listOf())
 
-    LazyColumn() {
-        items(quizList.size) { index ->
-            QuizItem(quizList[index]) { viewModel.translate(index) }
+    LazyColumn {
+        // keyを設定しないとquizのある要素が削除されるとquizListの全てがRecomposeされてしまう
+        items(
+            items = quizList,
+            key = { quiz ->
+                quiz.id
+            }
+        ) { quiz ->
+            QuizItem(quiz) { viewModel.translate(quiz.id) }
         }
         item {
             if (quizList.isNotEmpty() && canLoading) LoadingIndicator { viewModel.getQuizList() }
